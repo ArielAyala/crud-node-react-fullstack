49c62aa5bd7f3a5c69708f530090ad2b8ccbe71d
Ariel Ayala <arielayalamail@gmail.com>
2024-02-15 15:46:57 -0300

    User routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import { PORT } from "./config.js";
 
 import personaRoutes from "./routes/persona.routes.js";
 import habitacionRoutes from "./routes/habitacion.routes.js";
+import reservaRoutes from "./routes/reserva.routes.js";
 
 const app = express();
 app.use(express.json());
 
+
 app.use("/api", personaRoutes);
 app.use("/api", habitacionRoutes);
+app.use("/api", reservaRoutes)
 
 app.use((req, res) => {
   res.status(400).json({
