7bdc6f0eaf1fc750e2b9dd8f3c13cb78b183c49b
Ariel Ayala <arielayalamail@gmail.com>
2024-02-16 10:22:28 -0300

    Modal edit and create persona

--- frontend/src/components/Persona.jsx
+++ frontend/src/components/Persona.jsx
@@ -1,57 +1,90 @@
 import { useEffect, useState } from "react";
-import { getPersonas } from "../api/persona.service";
+import { getAllPersonas, createPersona } from "../api/persona.service";
 import { Modal, Button } from "react-bootstrap";
+import { Operation } from "../utils/operations";
 
 const Persona = () => {
   const [personas, setPersonas] = useState([]);
   const [show, setShow] = useState(false);
+  const [operationModal, setOperationModal] = useState(1);
+  const [modalTitle, setModalTitle] = useState("");
 
-  const handleCloseModal = () => setShow(false);
-  const handleShowModal = () => setShow(true);
+  const [id, setId] = useState("");
+  const [nombre, setNombreCompleto] = useState("");
+  const [documento, setDocumento] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [correo, setCorreo] = useState("");
 
   useEffect(() => {
-    getPersonas()
+    getPersonas();
+  }, []);
+
+  const getPersonas = () => {
+    getAllPersonas()
       .then((response) => {
         setPersonas(response);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  };
+
+  const closeModal = () => setShow(false);
+
+  const openModal = (op, id, nombreCompleto, documento, telefono, correo) => {
+    setShow(true);
+
+    setId("");
+    setNombreCompleto("");
+    setDocumento("");
+    setTelefono("");
+    setCorreo("");
+
+    setOperationModal(op);
+
+    if (op === Operation.CREATE) {
+      setModalTitle("Registrar persona");
+    } else if (op === Operation.EDIT) {
+      setModalTitle("Editar persona");
+      setId(id);
+      setNombreCompleto(nombreCompleto);
+      setDocumento(documento);
+      setTelefono(telefono);
+      setCorreo(correo);
+
+      window.setTimeout(() => {
+        document.getElementById("nombre").focus();
+      }, 500);
+    }
+  };
+
+  const addPersona = async () => {
+    const personaBody = {
+      nombre: nombre.trim(),
+      documento: documento.trim(),
+      telefono: telefono.trim(),
+      correo: correo.trim(),
+    };
+    const createPersonaResponse = await createPersona(personaBody);
+    if (createPersonaResponse) {
+      // ...
+    } else {
+      // ...
+    }
+  };
 
   return (
     <>
-      <Modal show={show} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <input type="hidden" id="id" />
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                Nombre:
-              </span>
-            </div>
-            <input type="text" className="form-control" />
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleCloseModal}>
-            Guardar
-          </Button>
-        </Modal.Footer>
-      </Modal>
       <div>
         <div className="row justify-content-between">
           <div className="col">
             <h3>Personas</h3>
           </div>
           <div className="col-auto">
-            <Button variant="primary" onClick={handleShowModal}>
+            <Button
+              variant="primary"
+              onClick={() => openModal(Operation.CREATE)}
+            >
               Registrar nueva persona
             </Button>
           </div>
@@ -74,7 +107,19 @@ const Persona = () => {
                 <td>{persona.telefono}</td>
                 <td>{persona.correo}</td>
                 <td>
-                  <button className="btn btn-warning">
+                  <button
+                    className="btn btn-warning"
+                    onClick={() =>
+                      openModal(
+                        Operation.EDIT,
+                        persona.id,
+                        persona.nombrecompleto,
+                        persona.nrodocumento,
+                        persona.telefono,
+                        persona.correo
+                      )
+                    }
+                  >
                     <i className="fa-solid fa-edit"></i>
                   </button>
                   &nbsp;
@@ -88,7 +133,62 @@ const Persona = () => {
         </table>
       </div>
 
-      {/* Modal */}
+      <Modal show={show} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="hidden" id="id" value={id} />
+          <div className="form-group mb-3">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              className="form-control"
+              id="nombre"
+              value={nombre}
+              onChange={(e) => setNombreCompleto(e.target.value)}
+            />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="ci">CI N°</label>
+            <input
+              type="texto"
+              className="form-control"
+              id="ci"
+              value={documento}
+              onChange={(e) => setDocumento(e.target.value)}
+            />
+          </div>
+          <div className="form-groupmb-3">
+            <label htmlFor="telefono">Teléfono</label>
+            <input
+              type="tel"
+              className="form-control"
+              id="telefono"
+              value={telefono}
+              onChange={(e) => setTelefono(e.target.value)}
+            />
+          </div>
+          <div className="form-groupmb-3">
+            <label htmlFor="correo">Correo</label>
+            <input
+              type="mail"
+              className="form-control"
+              id="correo"
+              value={correo}
+              onChange={(e) => setCorreo(e.target.value)}
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={() => addPersona()}>
+            Guardar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
