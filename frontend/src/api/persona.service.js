c1f52c4d135b88005607830df06f9059abeaa955
Ariel Ayala <arielayalamail@gmail.com>
2024-02-16 10:21:54 -0300

    Create  persona service - front

--- frontend/src/api/persona.service.js
+++ frontend/src/api/persona.service.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 import { API_URL } from "../config";
 
-export const getPersonas = async () => {
-  const response = await axios.get(API_URL + "personas/");
+const URL_ENTITY = API_URL + "personas/";
+
+export const getAllPersonas = async () => {
+  const response = await axios.get(URL_ENTITY);
   return response.data;
 };
 
 export const getPersonaById = async (id) => {
-  const response = await axios.get(API_URL + "personas/" + id);
+  const response = await axios.get(URL_ENTITY + id);
   return response.data;
+};
+
+export const createPersona = async (persona) => {
+  try {
+    const response = await axios.post(URL_ENTITY, persona);
+
+    if (response.status === 201) {
+      return response.data;
+    } else {
+      throw new Error("Error creating persona: " + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error creating persona:", error);
+    throw error;
+  }
 };
