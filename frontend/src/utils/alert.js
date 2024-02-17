ab2dfbef4349865d21a2bcef2f4ce0a4aad0bafe
Ariel Ayala <arielayalamail@gmail.com>
2024-02-17 01:47:45 -0300

    Confirmation dialog

--- frontend/src/utils/alert.js
+++ frontend/src/utils/alert.js
@@ -1,13 +1,26 @@
 import Swal from "sweetalert2";
 import whithReactContent from "sweetalert2-react-content";
 
-export const showAlert = (message, icon, focus = '') => {
+export const showAlert = (message, icon, focus = "") => {
   onfocus(focus);
   const MyAlert = whithReactContent(Swal);
   MyAlert.fire({
     title: message,
     icon,
   });
+};
+
+export const showConfirDeleteDialog = (message) => {
+  const MyAlert = whithReactContent(Swal);
+  return MyAlert.fire({
+    title: "",
+    text: message,
+    icon: ALERT_ICON.Question,
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Eliminar",
+  })
 };
 
 export const ALERT_ICON = {
