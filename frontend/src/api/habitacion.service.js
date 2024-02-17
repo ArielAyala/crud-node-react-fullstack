4cafac0306d25d3744c6926d631341fe47020d55
Ariel Ayala <arielayalamail@gmail.com>
2024-02-17 11:11:49 -0300

    Crud Habitacion

---
+++ frontend/src/api/habitacion.service.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { API_URL } from "../config";
+
+const URL_ENTITY = API_URL + "habitaciones/";
+
+export const getAllHabitaciones = async () => {
+  const response = await axios.get(URL_ENTITY);
+  return response.data;
+};
+
+export const getHabitacionById = async (id) => {
+  const response = await axios.get(URL_ENTITY + id);
+  return response.data;
+};
+
+export const createHabitacion = async (habitacion) => {
+  try {
+    const response = await axios.post(URL_ENTITY, habitacion);
+
+    if (response.status === 201) {
+      return response.data;
+    } else {
+      throw new Error("Error creating habitacion: " + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error creating habitacion:", error);
+    throw error;
+  }
+};
+
+export const updateHabitacion = async (id, habitacionData) => {
+  try {
+    const response = await axios.patch(URL_ENTITY + id, habitacionData);
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Error updating habitacion: " + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error updating habitacion:", error);
+    throw error;
+  }
+};
+
+export const deleteHabitacion = async (id) => {
+  try {
+    const response = await axios.delete(URL_ENTITY + id);
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Error deleting habitacion: " + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error deleting habitacion:", error);
+    throw error;
+  }
+};
--- frontend/src/components/Habitacion.jsx
+++ frontend/src/components/Habitacion.jsx
@@ -1,11 +1,314 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+  getAllHabitaciones,
+  createHabitacion,
+  updateHabitacion,
+  deleteHabitacion,
+} from "../api/habitacion.service";
+import { Operation } from "../utils/operations";
+import { showAlert, ALERT_ICON, showConfirDeleteDialog } from "../utils/alert";
+import { useForm } from "react-hook-form";
+import { Modal, Button } from "react-bootstrap";
 
 const Habitacion = () => {
-  return (
-    <div>
-      <h1>Habitacion</h1>
-    </div>
-  )
-}
+  const [habitaciones, setHabitaciones] = useState([]);
+  const [show, setShow] = useState(false);
+  const [operationModal, setOperationModal] = useState(1);
+  const [modalTitle, setModalTitle] = useState("");
 
-export default Habitacion
+  const [id, setId] = useState("");
+  const [habitacionPiso, setHabitacionPiso] = useState(1);
+  const [habitacionNro, setHabitacionNro] = useState(1);
+  const [cantCamas, setCantCamas] = useState(1);
+  const [tieneTelevision, setTieneTelevision] = useState(false);
+  const [tieneFrigobar, setTieneFrigobar] = useState(false);
+
+  // prettier-ignore
+  const {register, handleSubmit, formState: { errors }, reset} = useForm();
+
+  useEffect(() => {
+    getHabitaciones();
+  }, []);
+
+  const getHabitaciones = () => {
+    getAllHabitaciones()
+      .then((response) => {
+        setHabitaciones(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const closeModal = () => setShow(false);
+
+  const openModal = (
+    op,
+    id,
+    habitacionPiso,
+    habitacionNro,
+    cantCamas,
+    tieneTelevision,
+    tieneFrigobar
+  ) => {
+    setShow(true);
+    reset();
+    setOperationModal(op);
+
+    if (op === Operation.CREATE) {
+      setModalTitle("Registrar habitación");
+      setId("");
+      setHabitacionPiso(1);
+      setHabitacionNro(1);
+      setCantCamas(1);
+      setTieneTelevision(false);
+      setTieneFrigobar(false);
+    } else if (op === Operation.EDIT) {
+      setModalTitle("Editar habitación");
+      setId(id);
+      setHabitacionPiso(habitacionPiso);
+      setHabitacionNro(habitacionNro);
+      setCantCamas(cantCamas);
+      setTieneTelevision(tieneTelevision);
+      setTieneFrigobar(tieneFrigobar);
+
+      window.setTimeout(() => {
+        document.getElementById("habitacionPiso").focus();
+      }, 500);
+    }
+  };
+
+  const onSubmit = async () => {
+    const habitacionBody = {
+      habitacionPiso,
+      habitacionNro,
+      cantCamas,
+      tieneTelevision,
+      tieneFrigobar,
+    };
+
+    try {
+      if (operationModal === Operation.CREATE) {
+        const createHabitacionResponse = await createHabitacion(habitacionBody);
+        if (createHabitacionResponse) {
+          closeModal();
+          showAlert("Habitación registrada correctamente", ALERT_ICON.Success);
+          getHabitaciones();
+        }
+      } else if (operationModal === Operation.EDIT) {
+        const updateHabitacionResponse = await updateHabitacion(
+          id,
+          habitacionBody
+        );
+        if (updateHabitacionResponse) {
+          closeModal();
+          showAlert("Habitación actualizada correctamente", ALERT_ICON.Success);
+          getHabitaciones();
+        }
+      }
+    } catch (error) {
+      showAlert("Hubo un error", ALERT_ICON.Error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    const confirmDelete = await showConfirDeleteDialog(
+      "¿Está seguro de eliminar esta habitación?"
+    );
+    if (confirmDelete.isConfirmed) {
+      try {
+        await deleteHabitacion(id);
+        showAlert("Habitación eliminada correctamente", ALERT_ICON.Success);
+        getHabitaciones();
+      } catch (error) {
+        showAlert("Hubo un error al eliminar la habitación", ALERT_ICON.Error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <div className="row justify-content-between">
+          <div className="col">
+            <h3>Habitaciones</h3>
+          </div>
+          <div className="col-auto">
+            <Button
+              variant="primary"
+              onClick={() => openModal(Operation.CREATE)}
+            >
+              Registrar nueva habitación
+            </Button>
+          </div>
+        </div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Piso</th>
+              <th scope="col">Número</th>
+              <th scope="col">Camas</th>
+              <th scope="col">Televisión</th>
+              <th scope="col">Frigobar</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {habitaciones.map((habitacion, i) => (
+              <tr key={i}>
+                <td>{habitacion.habitacionpiso}</td>
+                <td>{habitacion.habitacionnro}</td>
+                <td>{habitacion.cantcamas}</td>
+                <td>{habitacion.tienetelevision ? "Sí" : "No"}</td>
+                <td>{habitacion.tienefrigobar ? "Sí" : "No"}</td>
+                <td>
+                  <button
+                    className="btn btn-warning"
+                    onClick={() =>
+                      openModal(
+                        Operation.EDIT,
+                        habitacion.id,
+                        habitacion.habitacionpiso,
+                        habitacion.habitacionnro,
+                        habitacion.cantcamas,
+                        habitacion.tienetelevision,
+                        habitacion.tienefrigobar
+                      )
+                    }
+                  >
+                    <i className="fa-solid fa-edit"></i>
+                  </button>
+                  &nbsp;
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(habitacion.id)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <Modal show={show} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <input type="hidden" id="id" value={id} />
+            <div className="form-group mb-3">
+              <label htmlFor="habitacionPiso">Piso</label>
+              <input
+                {...register("habitacionPiso", {
+                  required: "El piso es requerido",
+                })}
+                type="number"
+                className="form-control"
+                id="habitacionPiso"
+                value={habitacionPiso}
+                onChange={(e) =>
+                  setHabitacionPiso(parseInt(e.target.value, 10))
+                }
+                required
+                min={1}
+                max={10}
+              />
+              <div
+                className={`invalid-feedback${
+                  errors.habitacionPiso ? " d-block" : ""
+                }`}
+              >
+                {errors.habitacionPiso?.message}
+              </div>
+            </div>
+            <div className="form-group mb-3">
+              <label htmlFor="habitacionNro">Número</label>
+              <input
+                {...register("habitacionNro", {
+                  required: "El número de habitación es requerido",
+                })}
+                type="number"
+                className="form-control"
+                id="habitacionNro"
+                value={habitacionNro}
+                onChange={(e) => setHabitacionNro(parseInt(e.target.value, 10))}
+                required
+                min={1}
+                max={20}
+              />
+              <div
+                className={`invalid-feedback${
+                  errors.habitacionNro ? " d-block" : ""
+                }`}
+              >
+                {errors.habitacionNro?.message}
+              </div>
+            </div>
+            <div className="form-group mb-3">
+              <label htmlFor="cantCamas">Cantidad de Camas</label>
+              <input
+                {...register("cantCamas", {
+                  required: "La cantidad de camas es requerida",
+                })}
+                type="number"
+                className="form-control"
+                id="cantCamas"
+                value={cantCamas}
+                onChange={(e) => setCantCamas(parseInt(e.target.value, 10))}
+                required
+                min={1}
+                max={4}
+              />
+              <div
+                className={`invalid-feedback${
+                  errors.cantCamas ? " d-block" : ""
+                }`}
+              >
+                {errors.cantCamas?.message}
+              </div>
+            </div>
+            <div className="form-check mb-3">
+              <input
+                {...register("tieneTelevision")}
+                type="checkbox"
+                className="form-check-input"
+                id="tieneTelevision"
+                checked={tieneTelevision}
+                onChange={(e) => setTieneTelevision(e.target.checked)}
+              />
+              <label className="form-check-label" htmlFor="tieneTelevision">
+                ¿Tiene televisión?
+              </label>
+            </div>
+            <div className="form-check mb-3">
+              <input
+                {...register("tieneFrigobar")}
+                type="checkbox"
+                className="form-check-input"
+                id="tieneFrigobar"
+                checked={tieneFrigobar}
+                onChange={(e) => setTieneFrigobar(e.target.checked)}
+              />
+              <label className="form-check-label" htmlFor="tieneFrigobar">
+                ¿Tiene frigobar?
+              </label>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={handleSubmit(onSubmit)}>
+            Guardar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default Habitacion;
