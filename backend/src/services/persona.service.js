d3858b883df2ef29e5957da30bf5b886997eb64a
Ariel Ayala <arielayalamail@gmail.com>
2024-02-18 15:39:16 -0300

    Format code

--- backend/src/services/persona.service.js
+++ backend/src/services/persona.service.js
@@ -4,7 +4,6 @@ import { pool } from "../db.js";
 //import JoiValidationError from "../utils/joiValidationError.js";
 
 class PersonaService {
-
   async getPersonas() {
     const [rows] = await pool.query("SELECT * FROM persona");
     return rows;
@@ -20,27 +19,12 @@ class PersonaService {
     return rows[0];
   }
 
-  // async create(persona) {
-  //   const { nombre, documento, correo, telefono } = persona;
-
-  //   const [result] = await pool.query(
-  //     "INSERT INTO persona (nombre, email, password) VALUES (?, ?, ?)",
-  //     [nombre, email, password]
-  //   );
-
-  //   return {
-  //     id: result.insertId,
-  //     nombre,
-  //     email,
-  //   };
-  // }
-
   async createPersona(persona) {
     const schema = Joi.object({
       nombre: Joi.string().min(3).max(255).required(),
       documento: Joi.string().min(5).max(20).required(),
       telefono: Joi.string().min(0).max(20).required(),
-      correo: Joi.string().allow(null, '')
+      correo: Joi.string().allow(null, ""),
     });
 
     const { error } = schema.validate(persona);
@@ -67,12 +51,11 @@ class PersonaService {
   }
 
   async updatePersona(id, persona) {
-
     const schema = Joi.object({
       nombre: Joi.string().min(3).max(255).required(),
       documento: Joi.string().min(5).max(20).required(),
       telefono: Joi.string().min(0).max(20).required(),
-      correo: Joi.string().allow(null, '')
+      correo: Joi.string().allow(null, ""),
     });
 
     const { error } = schema.validate(persona);
