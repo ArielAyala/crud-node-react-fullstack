3d58a2028a0e5a9d05fdb9bcbef6cf63e54e427a
Ariel Ayala <arielayalamail@gmail.com>
2024-02-19 09:26:01 -0300

    Deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "crud-node-react-fullstack-production.up.railway.app/api/";
+export const API_URL = "https://crud-node-react-fullstack-production.up.railway.app/api/";
 export const LIMITES_RESERVA = {
     MaxPisos : 10,
     MaxHabitaciones : 20,
